bb9bd119a21a1750c1ed26d3c27faf26b585b002
KyialS <sadybakasova7@gmail.com>
2024-06-07 01:37:50 +0600

    second commit

--- backend/app.py
+++ backend/app.py
@@ -1,56 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask
+import os
+import psycopg2
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return '''
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Backend</title>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-                background-image: url('https://verpex.com/assets/uploads/images/blog/How-to-become-a-Backend-Developer.jpg?v=1665484477');
-                background-size: cover;
-                background-position: center;
-                margin: 0;
-            }
-            .container {
-                text-align: center;
-                padding: 20px;
-                background: rgba(255, 255, 255, 0.8);
-                border-radius: 8px;
-                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            }
-            h1 {
-                color: #00796b;
-            }
-            p {
-                color: #004d40;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <h1>Добро пожаловать на Backend</h1>
-            <p>Это страница сервера Flask</p>
-        </div>
-    </body>
-    </html>
-    '''
+def hello():
+    db_host = os.environ.get('DB_HOST')
+    db_name = os.environ.get('DB_NAME')
+    db_user = os.environ.get('DB_USER')
+    db_password = os.environ.get('DB_PASSWORD')
+    conn = psycopg2.connect(
+        host=db_host,
+        database=db_name,
+        user=db_user,
+        password=db_password
+    )
+    cur = conn.cursor()
+    cur.execute('SELECT version()')
+    db_version = cur.fetchone()
+    cur.close()
+    conn.close()
+    return f'Hello, World! DB Version: {db_version}'
 
-@app.route('/data')
-def data():
-    return jsonify(message="Hello from backend!")
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0')
-
